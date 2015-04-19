2776e298a1289e46487454425e35209dbe8238fc
Diego Souza Góes <diego.s.goes@gmail.com>
2015-04-19 14:35:28 -0300

    inclusão do input password

--- forka.php
+++ forka.php
@@ -7,7 +7,7 @@
 		require_once('forka.html');
 	?>
 	<div id='inputDiv'>
-		<h2>Descura qual é a palavra chave e salve NED da Forka</h2>
+		<h2>Descura qual é a palavra chave e salve NED da Forka!</h2>
 		<input id='inputText' type='text' size='2' maxlength='1'>
 		<button id='inputSbm'>Go</button>
 	</div>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<img src="images/logo.png" id="logo">
 		<p>O Rei JOFF condenou o nosso amigo NED à <i>forka</i>, mas talvez você possa ajuda-lo!</p>
 		<p>Escolha uma palavra</p>
-		<input id='passwordInp' type='text' size='15'>
+		<input id='passwordInp' type='password' size='15'>
 		<button id='startBtn'>Começar</button>
 		
 	</div>
